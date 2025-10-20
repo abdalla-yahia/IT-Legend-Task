636e0f3be64d66e7baf975e920c5122029809664
ابو يحيى عبدالله <abdalla-yahia>
2025-10-20 21:50:35 +0300

    add course title to header dynamic

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,15 +5,16 @@ import Provider_Contextes from "@/Contexts/Provider_Contextes";
 import data from '@/DB/Curriculm_Content.json';
 import { Course_Interface } from "@/Interfaces/Course_Interface";
 import { setCourses } from "@/Features/Slices/Course_Slice";
+import { usePathname } from "next/navigation";
 
 export default function MainLayout({children}: Readonly<{children: React.ReactNode;}>) {
-  
+  const pathname = usePathname();
   const dispatch = useAppDispatch();
   //Set Purchased Courses Data
   useEffect(()=>{
     const purchasedCourses = data?.filter((course:Course_Interface)=>course?.isPurchased);
     dispatch(setCourses(purchasedCourses))
-  },[])
+  },[pathname,dispatch])
 
   return (
     <>
