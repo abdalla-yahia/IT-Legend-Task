2a0753f3e47e5fbd1faa2620199f296d47247d70
Abdalla Yahia Kamel <94574716+abdalla-yahia@users.noreply.github.com>
2025-10-25 04:46:44 +0300

    Replace image with link and update heading text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,19 @@ export default function Home() {
   // },[])
   return (
       <main className="w-full h-screen  flex gap-[32px] justify-center items-center">
-        <Image          
-          className="dark:invert"
-          src="/favicon/android-chrome-512x512.png"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <Link href={'/'}>
+          <Image          
+            className="dark:invert"
+            src="/favicon/android-chrome-512x512.png"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+         </Link> 
         <h1 className="text-3xl cursor-pointer ">
           <Link href={'/courses'}>
-          It Legend Task
+          It Legend Courses
           </Link>
           </h1>
     </main>
